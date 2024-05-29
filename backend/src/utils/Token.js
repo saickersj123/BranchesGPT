7def7e7a7efd284ff152b1fdcf53640bd6c51501
saickersj123 <150012248+saickersj123@users.noreply.github.com>
2024-05-29 17:25:10 +0900

    Update Token.js

--- backend/src/utils/Token.js
+++ backend/src/utils/Token.js
@@ -12,7 +12,7 @@ async createToken(id, email, expiresIn) {
 },
 
 async verifyToken(req,res,next) {
-	const token = req.signedCookies[COOKIE_NAME]; // signed cookies is an object which can contain all of the cookies data
+	const token = req.signedCookies[`${COOKIE_NAME}`]; // signed cookies is an object which can contain all of the cookies data
 	console.log("token:",token);
 	if (!token || token.trim() === "") {
 		return res.status(401).json({ message: "Token Not Received" });
