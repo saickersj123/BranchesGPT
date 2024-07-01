f67ef8227c27bcee4c283f4ae2ae7471e37b5031
Flynn <150012248+saickersj123@users.noreply.github.com>
2024-07-02 03:29:27 +0900

    Update UserController.ts

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -61,6 +61,7 @@ export const userSignUp = async (
 			expires, // same as token expiration time
 			httpOnly: true,
 			signed: true,
+			sameSite: true,
 			secure: true,
 		});
 
@@ -116,6 +117,7 @@ export const userLogin = async (
 			expires, // same as token expiration time
 			httpOnly: true,
 			signed: true,
+			sameSite: true,
 			secure: true,
 		});
 
