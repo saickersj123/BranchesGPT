ef6d86ce2d7d3007625f55a3af5162a4f8c5c76e
Flynn <150012248+saickersj123@users.noreply.github.com>
2024-10-19 15:03:31 +0900

    hotfix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,8 +15,10 @@ config();
 const app = express();
 
 // Middlewares
-
-app.use(cors({origin: 'https://app.branchesgpt.o-r.kr'||'http://localhost:3000', credentials: true}));
+//for local testing
+//app.use(cors({origin: 'https://localhost:3000', credentials: true}));
+//for deployment
+app.use(cors({origin: 'https://app.branchesgpt.o-r.kr', credentials: true}));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(morgan("dev")); // for development
